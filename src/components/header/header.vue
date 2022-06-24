488b0c709f21e98eb4c08669febdfe9469d7ac2f
whjXMU <815037584@qq.com>
2022-06-25 01:33:02 +0800

    header_chiken_music

---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header">
+        <span class="icon"></span>
+        <h1 class="text"> Chicken Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  css: {
+    loaderOptions:{
+      sass: {
+        additionalData:`
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
